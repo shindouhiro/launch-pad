1076237fd2a3137997c0b8d3bd5adfe0cb8c97b7
Hikaru <shindouhiro@gmail.com>
2025-12-04 10:31:02 +0800

    chore: remove .next from git tracking

--- .next/dev/server/chunks/ssr/Desktop_ai_launch-pad__next-internal_server_app__not-found_page_actions_3d48027f.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Desktop/ai/launch-pad/.next-internal/server/app/_not-found/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Desktop_ai_launch-pad__next-internal_server_app__not-found_page_actions_3d48027f.js.map
--- .next/dev/server/chunks/ssr/Desktop_ai_launch-pad__next-internal_server_app_page_actions_62242c76.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Desktop/ai/launch-pad/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Desktop_ai_launch-pad__next-internal_server_app_page_actions_62242c76.js.map
